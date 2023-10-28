ad12b84c0d7cbd0be7a579b5d42781d08c933756
imran-py <imran.dtc144@gmail.com>
2023-10-28 15:56:52 +0530

    "main title" added below drawer using google font

--- src/componants/navbar/Navbar.js
+++ src/componants/navbar/Navbar.js
@@ -26,42 +26,54 @@ const Navbar = () => {
   const [openMenu,setOpenMenu] = useState(false)  
 
 //   elevation for shadow 0 for no shadow
-  return <AppBar color='default' elevation={0}> 
-        {/* app name */}
-        <StyledToolbar>
-            <Box flex={{ xs:25, md:1 }}>
-                <Typography variant='h4' color={'tomato'}>CodingStrade</Typography>
-            </Box>
-            {/* menu items */}
-            <MenuBox  flex={1} sx={{display:{xs:"none", md:'flex'}}}>
-                {MenuItems.map((item)=>(
-                    <Typography variant='body2'>{item.Name}</Typography>
-                ))}
-            </MenuBox>
-            {/* search box */}
-            <Box flex={1}>
-                <TextField sx={{display:{xs:"none", md:'flex'}}} label="search here" variant="outlined" color="warning" />
-                {/* add menu icon here  */}
-                {/* xs - extra small screen, md - for medium screen */}
-                <MenuIcon sx={{ display: {xs:"flex", md:"none"},cursor:"pointer"}}
-                onClick={()=>setOpenMenu(!openMenu)}></MenuIcon>
-            </Box>
-        </StyledToolbar>
-        {/* add drawer */}
-        <Drawer
-        //  open close drawer action here 
-          anchor={"top"} open={openMenu} onClose={()=>setOpenMenu(!openMenu)}
-         >
-          {/* items of drawer */}
-          <List>
-            <ListItem>
-            {MenuItems.map((item)=>(
-                    <ListItemButton variant='body2'>{item.Name}</ListItemButton>
-                ))}
-            </ListItem>
-          </List>
-        </Drawer>
-  </AppBar>
-}
-
+  return  (
+  <>
+    <AppBar color='default' position='sticky' elevation={0}> 
+          {/* app name */}
+          <StyledToolbar>
+              <Box flex={{ xs:25, md:1 }}>
+                  <Typography variant='h4' color={'tomato'}  sx={{fontFamily: 'Splash, cursive'}}>FoodApp</Typography>
+              </Box>
+              {/* menu items */}
+              <MenuBox  flex={1} sx={{display:{xs:"none", md:'flex'}}}>
+                  {MenuItems.map((item)=>(
+                      <Typography variant='body2'>{item.Name}</Typography>
+                  ))}
+              </MenuBox>
+              {/* search box */}
+              <Box flex={1}>
+                {/* add search box for medium and big screen */}
+                  <TextField sx={{display:{xs:"none", md:'flex'}}} label="search here" variant="outlined" color="warning" />
+                  {/* add menu icon here  */}
+                  {/* xs - extra small screen, md - for medium screen */}
+                  <MenuIcon sx={{ display: {xs:"flex", md:"none"},cursor:"pointer"}}
+                  onClick={()=>setOpenMenu(!openMenu)}></MenuIcon>
+              </Box>
+          </StyledToolbar>
+          {/* add drawer */}
+          <Drawer
+          //  open close drawer action here 
+            anchor={"top"} open={openMenu} onClose={()=>setOpenMenu(!openMenu)}
+          >
+            {/* items of drawer */}
+            <List>
+              <ListItem>
+              {MenuItems.map((item)=>(
+                      <ListItemButton variant='body2'>{item.Name}</ListItemButton>
+                  ))}
+              </ListItem>
+            </List>
+            {/* add search box for small screen */}
+            <TextField sx={{display:{xs:"flex", md:'none'}}} label="search here" variant="outlined" color="warning" />
+          </Drawer>
+          
+    </AppBar>
+    {/* add Main Title below drawer  */}
+    <Box sx={{display:'flex',justifyContent:'center'}}>
+      <Typography variant='h5' mr={1}>Simple Receipe for Coders</Typography>
+      <Typography variant='h5' color={'tomato'} sx={{fontFamily: 'Splash, cursive'}}>Simple Receipe for Coders</Typography>
+    </Box>
+  </>
+);
+};
 export default Navbar
