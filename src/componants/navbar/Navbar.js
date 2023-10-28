3240d2f50918346d44aa76669d2d92a33cb485a5
imran-py <imran.dtc144@gmail.com>
2023-10-28 14:11:36 +0530

    Menuitem added into Toolbar

--- src/componants/navbar/Navbar.js
+++ src/componants/navbar/Navbar.js
@@ -7,12 +7,31 @@ const Navbar = () => {
     justifyContent:'center',
     alignItems:'center'
   })
-  return <AppBar>
+
+  const MenuBox = styled(Box)({
+    display:'flex',
+    gap:30,
+    cursor:'pointer'
+    })
+
+  const MenuItems = [
+    {Name:"Home",Link:'/'},
+    {Name:"About us",Link:'#'},
+    {Name:"Contact us",Link:'#'},
+    {Name:"Subscribe",Link:'#'}]
+
+//   elevation for shadow 0 for no shadow
+  return <AppBar color='default' elevation={0}> 
 
         <StyledToolbar>
-            <Box>
+            <Box flex={1}>
                 <Typography variant='h4' color={'tomato'}>CodingStrade</Typography>
             </Box>
+            <MenuBox  flex={1}>
+                {MenuItems.map((item)=>(
+                    <Typography variant='body2'>{item.Name}</Typography>
+                ))}
+            </MenuBox>
         </StyledToolbar>
   </AppBar>
 }
