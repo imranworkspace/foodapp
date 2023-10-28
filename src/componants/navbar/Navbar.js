259fe06f59eea45a05015dd64214a6a400bc0775
imran-py <imran.dtc144@gmail.com>
2023-10-28 15:12:23 +0530

    add sizes for small devices

--- src/componants/navbar/Navbar.js
+++ src/componants/navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { AppBar, Box, Toolbar, Typography, styled } from '@mui/material'
+import TextField from '@mui/material/TextField';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
   const StyledToolbar = styled(Toolbar)({
@@ -22,16 +24,24 @@ const Navbar = () => {
 
 //   elevation for shadow 0 for no shadow
   return <AppBar color='default' elevation={0}> 
-
+        {/* app name */}
         <StyledToolbar>
-            <Box flex={1}>
+            <Box flex={{ xs:25, md:1 }}>
                 <Typography variant='h4' color={'tomato'}>CodingStrade</Typography>
             </Box>
-            <MenuBox  flex={1}>
+            {/* menu items */}
+            <MenuBox  flex={1} sx={{display:{xs:"none", md:'flex'}}}>
                 {MenuItems.map((item)=>(
                     <Typography variant='body2'>{item.Name}</Typography>
                 ))}
             </MenuBox>
+            {/* search box */}
+            <Box flex={1}>
+                <TextField sx={{display:{xs:"none", md:'flex'}}} label="search here" variant="outlined" color="warning" />
+                {/* add menu icon here  */}
+                {/* xs - extra small screen, md - for medium screen */}
+                <MenuIcon sx={{ display: {xs:"flex", md:"none"},cursor:"pointer"}}></MenuIcon>
+            </Box>
         </StyledToolbar>
   </AppBar>
 }
